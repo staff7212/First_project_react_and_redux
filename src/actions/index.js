f13f3016bc7ced00c21b078035f98b72fb27c2ea
Saveliy <staff7212@gmail.com>
2022-04-17 22:01:12 +0300

    Toolkit. createAction()

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 
+import { createAction } from "@reduxjs/toolkit";
 
 export const fetchHeroes = (request) => (dispatch) => {
   dispatch(heroesFetching());
@@ -14,24 +15,30 @@ export const fetchFilters = (request) => (dispatch) => {
     .catch(() => dispatch(filtersFetchingError()));
 };
 
-export const heroesFetching = () => {
-  return {
-    type: 'HEROES_FETCHING'
-  };
-};
+// export const heroesFetching = () => {
+//   return {
+//     type: 'HEROES_FETCHING'
+//   };
+// };
 
-export const heroesFetched = (heroes) => {
-  return {
-    type: 'HEROES_FETCHED',
-    payload: heroes
-  };
-};
+export const heroesFetching = createAction('HEROES_FETCHING')
 
-export const heroesFetchingError = () => {
-  return {
-    type: 'HEROES_FETCHING_ERROR'
-  };
-};
+// export const heroesFetched = (heroes) => {
+//   return {
+//     type: 'HEROES_FETCHED',
+//     payload: heroes
+//   };
+// };
+
+export const heroesFetched = createAction('HEROES_FETCHED')
+
+// export const heroesFetchingError = () => {
+//   return {
+//     type: 'HEROES_FETCHING_ERROR'
+//   };
+// };
+
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR')
 
 export const filtersFetching = () => {
   return {
